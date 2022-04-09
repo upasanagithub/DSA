ebffc1d3f752bb5ec89157383462b72fff38b498
Upasana Mahanta <ups.win@gmail.com>
2022-04-09 12:27:57 +0530

    Update checkIfPrime.js

--- checkIfPrime.js
+++ checkIfPrime.js
@@ -1,12 +1,12 @@
 /*Prime no.:- A no. havving exactly two factors, 1 and itself.
 Time Complexity:- O(sqroot(n))*/
 
-var isPrime = checkIfPrime(7);
+var isPrime = checkIfPrime(100);
 console.log(isPrime);
 
 function checkIfPrime(n){
     var c = 0;
-    for(var i=2; i*i<n; i++){
+    for(var i=2; i*i<=n; i++){
         if(n%i == 0){
             c++;
            if(i!=n/i){//optional, add if you want to print the factors apart from 1 and itself

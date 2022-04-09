3e0ab1e562eddcf3744d2d550857580ef2d44b89
Upasana Mahanta <ups.win@gmail.com>
2022-04-09 12:28:04 +0530

    Update findSqRoot.js

--- findSqRoot.js
+++ findSqRoot.js
@@ -1,3 +1,26 @@
 /* Given a perfect square n, find the square root of n 
+Time complexity: O(n)
+*/
 
 var sqRoot = findSqRoot(36);
+console.log(sqRoot);
+var sqRootOptimized = findSqRootOptimised(36);
+console.log(sqRootOptimized);
+
+function findSqRoot(n){
+   for(var i=1; i<=n; i++){
+       if(i*i == n){
+           return i;
+       }
+   }
+}
+
+/* Time complexity: O(sqroot(n))*/
+
+function findSqRootOptimised(n){
+    for(var i=1; i*i<=n; i++){
+        if(n%i == 0 && i==n/i){
+            return i;
+        }
+    }
+ }
